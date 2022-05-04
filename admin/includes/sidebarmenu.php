8350cba34a19f81d69ec6292ce48db97a90affb1
HTET HTET AUNG <83899503+HTET-88@users.noreply.github.com>
2022-05-05 00:50:43 +0630

    first

---
+++ admin/includes/sidebarmenu.php
@@ -0,0 +1,26 @@
+<div class="sidebar-menu" style="background-color: #28334B;">
+					<header class="logo1">
+						<a href="#" class="sidebar-icon"> <span class="fa fa-bars"></span> </a> 
+					</header>
+						<div style="border-top:1px ridge rgba(255, 255, 255, 0.15)"></div>
+                           <div class="menu">
+									<ul id="menu" >
+										<li><a href="dashboard.php"><i class="fa fa-tachometer"></i> <span>Dashboard</span><div class="clearfix"></div></a></li>
+										
+									 <li id="menu-academico" ><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i><span> Manager Courses</span> <span class="fa fa-angle-right" style="float: right"></span><div class="clearfix"></div></a>
+										   <ul id="menu-academico-sub" >
+										   <li id="menu-academico-avaliacoes" ><a href="add.php">Add </a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage.php">Manage</a></li>
+										  </ul>
+										</li>
+									<li id="menu-academico" ><a href="users.php"><i class="fa fa-users" aria-hidden="true"></i><span>Manage Users</span><div class="clearfix"></div></a></li>
+									
+									<li><a href="enroll.php"><i class="fa fa-list" aria-hidden="true"></i>  <span>Manage Enrollment</span><div class="clearfix"></div></a></li>
+									 <li><a href="appointment.php"><i class="fa fa-table"></i>  <span>Manage appointment</span><div class="clearfix"></div></a></li>
+									<li><a href="manage-enquires.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>  <span>Manage Enquiries</span><div class="clearfix"></div></a></li>
+									<li><a href="manage-pages.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>  <span>Manage Pages</span><div class="clearfix"></div></a></li>
+							     
+									
+								  </ul>
+								</div>
+							  </div>
